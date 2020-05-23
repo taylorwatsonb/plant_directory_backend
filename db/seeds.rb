ec669f6552cb2052ce1fb145749f2f8f01b17022
github taylorwatsonb <github taylorwatsonb@gmail.com>
2020-05-23 02:30:44 -0400

    plant model and migration and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+flower = Property.create(name: "Flower")
+root = Property.create(name: "Root")
+leaf = Property.create(name: "Leaf")
